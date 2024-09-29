d3cbb587bd582d381a9c2262ca136563a4cb17a9
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-09-29 16:45:57 -0700

    Use style.css and icon for dropdown
    dropdown hidden. WIP button reveals dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert('hi');
+import dropdownIcon from './images/dropdownButton.svg';
+import './style.css';
+
+const dropdownImage = document.getElementById('dropdownImage');
+dropdownImage.src = dropdownIcon;
