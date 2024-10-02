8caf82683058767afb87ff039d8a9acaa1a4e6f1
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-10-02 00:54:01 -0700

    Make custom dropdown and button
    Use div and a for custom visual dropdown instead of select. Add code that shows dropdown when button clicked.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import dropdownIcon from './images/dropdownButton.svg';
 import './style.css';
 
-const dropdownImage = document.getElementById('dropdownImage');
+const dropdownImage = document.querySelector('.dropdownImage')
 dropdownImage.src = dropdownIcon;
+
+function showDropdown () {
+    const dropdownContent = document.querySelector('.dropdownContent');
+    dropdownContent.classList.toggle('show');
+}
+
+const dropdownButton = document.querySelector('.dropdownButton');
+dropdownButton.addEventListener('click', (e) => {
+    showDropdown();
+});
