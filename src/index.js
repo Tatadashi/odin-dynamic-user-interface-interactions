32767c498246c7bfb9359248b9ee01d5191621e9
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-10-02 18:24:27 -0700

    Add js to close dropdown Close dropdown if clicked outside of button

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './style.css';
 const dropdownImage = document.querySelector('.dropdownImage')
 dropdownImage.src = dropdownIcon;
 
-function showDropdown () {
+function toggleDropdown () {
     const dropdownContent = document.querySelector('.dropdownContent');
     dropdownContent.classList.toggle('show');
 }
 
 const dropdownButton = document.querySelector('.dropdownButton');
 dropdownButton.addEventListener('click', (e) => {
-    showDropdown();
+    toggleDropdown();
+});
+
+window.addEventListener('click', (e) => {
+    if (!e.target.matches('.dropdownButton')) {
+        const dropdownContent = document.querySelector('.dropdownContent');
+        if (dropdownContent.classList.contains('show')) {
+            toggleDropdown();
+        }
+    }
 });
