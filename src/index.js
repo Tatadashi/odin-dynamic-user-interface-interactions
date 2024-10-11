2e2917eb1f5993a113e5e76168b944517e0f93dc
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-10-10 21:06:43 -0700

    Add functions to move slides back and forth
    WIP: connect fns to buttons and make it auto move forwards every inactive 5s

--- src/index.js
+++ src/index.js
@@ -14,5 +14,20 @@ function addCarouselSlideImage (elementID, src) {
     const element = document.getElementById(`${elementID}`);
     const image = new Image();
     image.src = src;
+    image.alt = elementID;
     element.appendChild(image);
+}
+
+function changeCurrentSlideForward () {
+    const slides = document.getElementById('slides');
+    const currentSlide = document.querySelector('.slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    slides.prepend(nextSlide);
+    slides.appendChild(currentSlide);
+}
+
+function changeCurrentSlideBack () {
+    const slides = document.getElementById('slides');
+    const lastSlide = slides.lastElementChild;
+    slides.prepend(lastSlide);
 }
